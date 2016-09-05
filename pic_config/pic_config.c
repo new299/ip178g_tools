87c5c1c23542663360d5c864e93516691d9c446f
Nava Whiteford <new@sgenomics.org>
2016-09-05 07:31:03 -0400

    hacking away at this mess

--- pic_config/pic_config.c
+++ pic_config/pic_config.c
@@ -4,16 +4,20 @@
 
 __code uint16_t __at (_CONFIG) __configword = _INTRC_OSC_NOCLKOUT & _WDTE_OFF;
 
-int clock_pin = 4; // GP4
-int data_pin  = 3; // GP3
+#define CLOCKPIN    0b00010000; // GP4
+#define DATAPIN     0b00000100; // GP3
 
-int width=100;
+#define NOTCLOCKPIN 0b11101111; // GP4
+#define NOTDATAPIN  0b11111011; // GP3
+
+int width=1000;
+int widthhalf=500;
  
 #define OUTPUT 0
 #define INPUT  1
 
-void delay(int t) {
-  for(int n=0;n<t*20;n++);
+void ndelay(int t) {
+  for(int n=0;n<t;n++);
 }
 
 void write(int p,int v) {
@@ -21,102 +25,36 @@ void write(int p,int v) {
   if(v == 0) GPIO = GPIO & (0xFF^(1 << p));
 }
 
-void pinMode(int p,int dir) {
-  if(dir == OUTPUT) TRISIO=0;
-  if(dir == INPUT ) TRISIO=0xFF;
-}
-
-/*
-int read(int p) {
-  if(GPIO & (1 << p)) return 1; else return 0;
-}
-
-int recv_one_bit() {
-  int data=0;
-  write(clock_pin,0);
-  delay(width);
-  write(clock_pin,1);
-  delay(width/2);
-  data = read(data_pin);
-  delay(width/2);
-  write(clock_pin,0);
-  return data;
-}
-*/
- 
 void send_data_bit(int data) {
-  write(clock_pin,0);
-  delay(width/2);
-  write(data_pin,data);
-  delay(width/2);
-  write(clock_pin,1);
-  delay(width);
-  write(clock_pin,0);
+  char n=0;
+  GPIO = 0;                 // CLK 0
+  for(n=0;n<255;n++);  // DELAY
+  if(data == 1) GPIO = DATAPIN; // DATA 1 (otherwise do nothing)
+  for(n=0;n<255;n++);  // DELAY
+  GPIO = GPIO | CLOCKPIN; // CLK 1
+  for(n=0;n<255;n++);  // DELAY
+  for(n=0;n<255;n++);  // DELAY
+  GPIO = GPIO & NOTCLOCKPIN; // CLK 0
 }
  
 
 void outhigh() {
-  pinMode(data_pin,OUTPUT);
-  write(data_pin,1);
-  write(clock_pin,0);
+  GPIO = DATAPIN;
 }
  
-void send_data(uint32_t data,int32_t len) {
+void send_data(uint16_t data,int16_t len) {
  
-  for(int32_t n=len-1;n>=0;n--) {
-    if(data & ((uint32_t)1 << n)) {
+  for(int16_t n=len-1;n>=0;n--) {
+    if(data & ((uint16_t)1 << n)) {
       send_data_bit(1);
     } else {
       send_data_bit(0);
     }
   }
 }
-
-/*
-uint32_t recv_data(int32_t len) {
  
-  uint32_t datain = 0;
-  for(int32_t n=(len-1);n>=0;n--) {
-    int d = recv_one_bit();
-    if(d != 0) {
-      datain = datain | (uint32_t)((uint32_t)1 << n);
-    }
-  }
-  return datain;
-}
- 
-uint32_t read_op(int phy,int reg) {
-  int inA;
-  uint32_t inp;
+void write_op(int phy,int reg,uint16_t data) {
    
-  pinMode(data_pin,OUTPUT);
-  send_data_bit(1);
-   
-  send_data_bit(0); // start bits
-  send_data_bit(1);
-  send_data_bit(1); // read op
-  send_data_bit(0);
-   
-  send_data(phy,5);
-  send_data(reg,5);
- 
-  pinMode(data_pin,INPUT);
-   
-  inA = recv_one_bit(); // turn around
-  //int inB = recv_one_bit(); // this should be required as per spec, but a bit gets missed if I use it.
-   
-  inp = recv_data(16);
- 
-  outhigh();
-   
-  return inp;
-}
-*/
- 
- 
-void write_op(int phy,int reg,uint32_t data) {
-   
-  pinMode(data_pin,OUTPUT);
   send_data_bit(1);
  
   send_data_bit(0);
@@ -142,17 +80,18 @@ void write_op(int phy,int reg,uint32_t data) {
 //   monitoring port, res, mirrored port tx
 //4: 001 ,00000,00000001  
  
-uint32_t reg3 = 0xE001;
-uint32_t reg4 = 0x2001;
+uint16_t reg3 = 0xE001;
+uint16_t reg4 = 0x2001;
 
 void main() {
 
+  TRISIO=0;
   // Configure port
 
   outhigh();
-  delay(5000); // five seconds
+  ndelay(100000); // five seconds
    
   write_op(20,3,reg3);
   write_op(20,4,reg4);
-  for(int n=0;n<5;n++) for(int m=0;m<60;m++) delay(1000); // 5mins
+  for(int n=0;n<5;n++) for(int m=0;m<60;m++) ndelay(20000); // 5mins
 }
